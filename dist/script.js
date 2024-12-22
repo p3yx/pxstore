493a1ab3592ddd2524ec35a4c0cbadf448182ab8
PEYX <pxanime4@gmail.com>
2024-12-22 19:54:42 +0700

    Add files via upload

--- dist/script.js
+++ dist/script.js
@@ -2,13 +2,13 @@ function sendToWhatsapp(){
 	let number = "+6283151636921";
 
 	let name = document.getElementById('name').value;
-	let Config = document.getElementById('config').value;
-	let Server= document.getElementById('server').value;
+	let email = document.getElementById('email').value;
+	let message = document.getElementById('message').value;
 
 	var url = "https://wa.me/" + number + "?text="
-	+ "Name : " +name+ "%0a"
-	+ "Config : " +config+ "%0a"
-	+ "Server : " +server+ "%0a%0a";
-
+	
+	+ "- *Name  :* " +name+ "%0a"
+	+ "- *Config :* " +email+ "%0a"
+	+ "- *Server  :* " +message+ "%0a%0a";
 	window.open(url, '_blank').focus();
 }
