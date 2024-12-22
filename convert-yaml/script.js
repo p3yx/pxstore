cd13a9456c392c297fb8b6263c2afa9f80e01ea6
PEYX <pxanime4@gmail.com>
2024-12-22 21:07:24 +0700

    Add files via upload

--- convert-yaml/script.js
+++ convert-yaml/script.js
@@ -1,19 +1,27 @@
 const form = document.getElementById('converter-form');
-const v2rayConfig = document.getElementById('v2ray-config');
+const v2rayUri = document.getElementById('v2ray-uri');
 const yamlOutput = document.getElementById('yaml-output');
 const convertBtn = document.getElementById('convert-btn');
 
 convertBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const v2rayJson = JSON.parse(v2rayConfig.value);
-  const yamlString = jsYaml.dump(v2rayJson);
+  const uri = v2rayUri.value;
+  const params = uri.split('?')[1].split('&');
+  const config = {};
+
+  params.forEach((param) => {
+    const [key, value] = param.split('=');
+    config[key] = value;
+  });
+
+  const yamlString = `
+server: ${config.server}
+port: ${config.port}
+type: ${config.type}
+tls: ${config.tls}
+network: ${config.network}
+uuid: ${config.uuid}
+`;
+
   yamlOutput.value = yamlString;
 });
-
-// Library js-yaml
-const jsYaml = {
-  dump: (obj) => {
-    return Object.keys(obj).map((key) => {
-      return `${key}: ${JSON.stringify(obj[key])}`;
-    }).join('\n');
-  }
